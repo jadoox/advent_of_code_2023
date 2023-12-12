18742036228a1f081d3765a88897f92f06b97c87
jadoox <jadoox@DESKTOP-QTKQISG>
2023-12-12 08:40:10 +0100

    more changes

--- day1/part2.py
+++ day1/part2.py
@@ -5,20 +5,61 @@ file_obj = file.readlines()
 for line in file_obj:
   calibration_vls.append(line.strip())
 
-# if the first 5 characters contains (one, two, three, four, five, six, seven, eight or nine) append to list, else append isdigit
-# if the last 5 characters contains (one, two, three, four, five, six, seven, eight or nine) append to list, else append isdigit
-lett_list=["one","two","three","four","five","six","seven","eight","nine"]
-num_list=[]
+last_list=[]
+
 for item in calibration_vls:
-    for lett in lett_list:
-        item.find(lett)
+    num_str=""
+    if item[0].isdigit():
+        num_str += str(item[0])
+    if item[-1].isdigit():
+        num_str += str(item[-1])
 
-# last_list=[]
-# for item in num_list:
-#     var = item[0] + item[-1]
-#     var = int(var)
-#     last_list.append(var)
+    if item[:3] == "one":
+        num_str += "1"
+    if item[-3:] == "one":
+        num_str += "1"
 
-# total = sum(last_list)
+    if item[:3] == "two":
+        num_str += "2"
+    if item[-3:] == "two":
+        num_str += "2"
 
-# print(total)
+    if item[:5] == "three":
+        num_str += "3"
+    if item[-5:] == "three":
+        num_str += "3"
+
+    if item[:4] == "four":
+        num_str += "4"
+    if item[-4:] == "four":
+        num_str += "4"
+
+    if item[:4] == "five":
+        num_str += "5"
+    if item[-4:] == "five":
+        num_str += "5"
+
+    if item[:3] == "six":
+        num_str += "6"
+    if item[-3:] == "six":
+        num_str += "6"
+
+    if item[:5] == "seven":
+        num_str += "7"
+    if item[-5:] == "seven":
+        num_str += "7"
+
+    if item[:5] == "eight":
+        num_str += "8"
+    if item[-5:] == "eight":
+        num_str += "8"
+
+    if item[:4] == "nine":
+        num_str += "9"
+    if item[-4:] == "nine":
+        num_str += "9"
+
+    # num_str = int(num_str)
+    last_list.append(num_str)
+
+print(num_str)
